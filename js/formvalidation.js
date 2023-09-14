922322ecaae1703b3a40c355ac1ce2a241b94c8d
jaydip2210 <rathodjaydip86738@gmail.com>
2023-09-14 10:19:23 +0530

    form_validation_done

--- js/formvalidation.js
+++ js/formvalidation.js
@@ -2,7 +2,36 @@ function validateForm() {
     // console.log("aa");
 
     let name = document.contactForm.name.value;
-    console.log(name);
+    // console.log(name);
+    let email = document.contactForm.email.value;
+    // console.log(email);
+    let mobile = document.contactForm.mobile.value;
+    // console.log(mobile);
+    let country = document.contactForm.country.value;
+    // console.log(country);
+    let gender = document.contactForm.gender.value;
+    // console.log(gender);
+    let hobby = document.contactForm.hobbies;
+    // console.log(hobbies);
+
+    let flag = false;
+
+    for (let i=0; i<hobby.length; i++) {
+        console.log(hobby[i].value, hobby[i].checked);
+
+        if (hobby[i].checked) {
+            flag = true;
+            break;
+        }
+    }
+
+    if (flag) {
+        document.getElementById("hobbyErr").innerHTML = ""
+    } else {
+        document.getElementById("hobbyErr").innerHTML = "Please select Altist one hobby"
+    }
+
+  
 
     if (name === '') {
         document.getElementById("nameErr").innerHTML = "Please enter your name";
@@ -18,9 +47,7 @@ function validateForm() {
         // document.getElementById("nameErr").innerHTML = " ";
     }
 
-    let email = document.contactForm.email.value;
-    console.log(email);
-
+  
     if (email === '') {
         document.getElementById("emailErr").innerHTML = "Please enter your email";
     } else {
@@ -35,23 +62,32 @@ function validateForm() {
         // document.getElementById("emailErr").innerHTML = " ";
     }
 
-    let mobile = document.contactForm.mobile.value;
-    console.log(mobile);
-
+  
     if (mobile === '') {
         document.getElementById("mobileErr").innerHTML = "Please enter your mobile naumber";
     } else {
+        let mobileReg = /^([0|\+[0-9]{1,5})?([7-9][0-9]{9})$/;
 
-        document.getElementById("mobileErr").innerHTML = " ";
+        if (mobileReg.test(mobile)) {
+            document.getElementById("mobileErr").innerHTML = " ";
+        } else {
+            document.getElementById("mobileErr").innerHTML = "Please enter valid number";
+        }
+
+        // document.getElementById("mobileErr").innerHTML = " ";
     }
 
-    let country = document.contactForm.country.value;
-    console.log(country);
-
-    if (country === country) {
+   
+    if (country === "0") {
         document.getElementById("countryErr").innerHTML = "Please select your country";
     } else {
         document.getElementById("countryErr").innerHTML = " ";
+    }
+
+    if(gender === '') {
+        document.getElementById("genderErr").innerHTML = "Please select your gender"
+    } else {
+        document.getElementById("genderErr").innerHTML = ""
     }
 
     return false;
