772cea17d6e801d2219235b466c141964c5d5499
jaydip2210 <rathodjaydip86738@gmail.com>
2023-09-23 17:58:05 +0530

    23_9_23_test

---
+++ js/Array.js
@@ -0,0 +1,57 @@
+// let arr = [15,8,99,66.7,'jaydip',75,33];
+
+// for(let i=0; i<arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// arr.forEach((value,index) => {
+//     console.log(index,value);
+// })
+
+// arr.map((v,i) => {
+//     console.log(i,v);
+// })
+
+// arr.push(10);
+// console.log(arr); //Last Add
+
+// arr.unshift(20);
+// console.log(arr); //First Add
+
+// arr.splice(2, 0, 30,40,50)  //Add 30 40 50 from 2nd index
+// console.log(arr);  
+
+//Remove
+// arr.pop();  
+// console.log(arr); //Remove Last
+
+// arr.shift();
+// console.log(arr); //Remove first
+
+// arr.splice(2, 3);
+// console.log(arr); //Remove 3 element from 2nd index //remove specifice
+
+// let arr1 = [500, 1000];
+// ans = arr.concat(arr1);
+// console.log(ans);
+
+// let ans = arr.toString();
+// console.log(ans);
+
+// let arr = [10,8,99,4,63];
+
+// let fData = arr.filter((v,i) => v > 18 && v < 70);
+// console.log(fData);
+
+// let arr = [15,8,99,75,33];
+// let res = arr.reduce((acc,v,i) => acc+v ,0);
+// console.log(res);
+
+// let res = arr.join("|");
+// console.log(res);
+
+// let res = arr.slice(1,3);
+// console.log(res);
+
+// let res = arr.fill("ok",2,4);
+// console.log(res);
---
+++ js/test2.js
@@ -0,0 +1,66 @@
+function handlesubmit() {
+    // console.log("111");
+
+    let name = document.getElementById("name").value;
+    let birth = document.getElementById("birth").value;
+    let mobile = document.getElementById("mobile").value;
+    let occupation = document.getElementById("occupation").value;
+    let income = document.getElementById("income").value;
+    let amount = document.getElementById("amount").value;
+    let smoke = document.getElementById("smoke").value;
+    console.log(income,amount,smoke);
+
+    console.log(occupation);
+
+    console.log(name, birth, mobile);
+
+    let age = new Date(birth)
+    let d = 2023 - age.getFullYear();
+    console.log(d);
+
+    let premium ;
+
+    if(d > 18 && d < 25) {
+        premium = 1000;
+    } else if (d > 26 && d < 35) {
+        premium = 1500;
+    } else if (d > 36 && d < 45) {
+        premium = 2000;
+    } else if (d > 45) {
+        premium = 2500;
+    }
+
+    console.log(premium);
+
+    let ePremium;
+
+    if (occupation === 'Self Employee') {
+        ePremium = premium * 0.10;
+    }
+    console.log(ePremium);
+
+    let insurance;
+
+    if (income === "0_to_3") {
+        if (amount > "1Lakh" && amount < "50Lakh") {
+            insurance =  amount;
+        } else if (amount > "50Lakh") {
+            insurance = "Please enter valid amount"
+        } 
+    } else if (income === "3_to_5") {
+        if (amount > "1Lakh" && amount < "1Cr") {
+            insurance =  amount;
+        } else if (amount > "1Cr") {
+            insurance = "Please enter valid amount"
+        } 
+    } else if (income === "3_to_5") {
+        if (amount > "1Lakh" && amount < "1Cr") {
+            insurance =  amount;
+        } else if (amount > "1Cr") {
+            insurance = "Please enter valid amount"
+        } 
+    }
+    console.log(insurance);
+
+    return false;
+}
